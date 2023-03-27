63f2c669de803ca18f20bcaebf25e85482ef19e5
krongpobm <128776545+krongpobm@users.noreply.github.com>
2023-03-28 04:05:53 +0700

    testadd project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,9 @@ import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';  
 import { ProductModule } from './product/product.module';
 import { UserModule } from './User/user.module';
-
+import { ProjectModule } from './project/project.module';
 @Module({
-  imports: [TodoModule, ProductModule, UserModule,],
+  imports: [TodoModule, ProductModule, UserModule, ProjectModule,],
   controllers: [AppController],
   providers: [AppService],
 })
